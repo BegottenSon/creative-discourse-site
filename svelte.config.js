19a6fdd039c25598df7a3f54d64ee44dc4a4d824
BegottenSon <danielbrown2d@gmail.com>
2021-09-20 13:38:29 -0400

    fix netlify

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,6 @@ import netlify from '@sveltejs/adapter-netlify';
 const config = {
 	kit: {
 		adapter: netlify(),
-		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
 };
